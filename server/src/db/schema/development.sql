6cba41125d3dafe1f6bc633b0047d4cdd12d3374
Johnny <johnnylabedzki@hotmail.com>
2021-10-21 16:50:20 -0700

    Finish insert statements for users and projects

--- server/src/db/schema/development.sql
+++ server/src/db/schema/development.sql
@@ -1,4 +1,20 @@
+INSERT INTO users(name, avatar)
+VALUES
+  ("Johnny", "https://i.imgur.com/31MtvRN.png")
+  ("Carrie", "https://i.imgur.com/qn5TFDx.png")
+  ("Jose", "https://i.imgur.com/31MtvRN.png")
+  ("David", "https://i.imgur.com/31MtvRN.png")
+  ("Amanda", "https://i.imgur.com/qn5TFDx.png")
+  ("Matthew", "https://i.imgur.com/31MtvRN.png")
+  ("Lydia Miller-Jones", "https://i.imgur.com/qn5TFDx.png")
+  ("Archie", "https://i.imgur.com/31MtvRN.png")
+  ("Justin", "https://i.imgur.com/31MtvRN.png");
 
+INSERT INTO projects (creator_id, name, description, start_date, end_date, background_img, status)
+VALUES
+  (2, "Final Project", "Create a tech-focused project management system with an integrated virtual meeting room", "2021-10-21", "2021-11-04", "")
+  (5, "Refactor legacy code", "Legacy code test coverage is currently only 20% and we introduce new bugs every time we add features. Let's change that with a complete refactor.", "2021-03-10", "2022-03-10", "")
+  (1, "Continued education", "This purpose of this project is to allocate time blocks for everyone to advance their learning and keep their knowledge up to date", "2022-01-02", "2022-02-01", "")
+  (3, "Put-it-on-a-tee", "Ecommerce website for client that sells custom tshirts", "2021-04-18", "2021-06-12", "", "Complete");
 
-INSERT INTO project(title, description, person_id)
-VALUES ('TEST PROJECT', 'Generic Description', 1);
+INSERT INTO tasks ()  

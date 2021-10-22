60ff7c2a98195569bdd99aba35ebb4fefa669f8b
Johnny <johnnylabedzki@hotmail.com>
2021-10-21 19:00:52 -0700

    Finish development seed, add 'npm run db:reset' script to package.json

--- server/src/db/schema/create.sql
+++ server/src/db/schema/create.sql
@@ -1,13 +1,13 @@
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS oauth_mapping;
-DROP TABLE IF EXISTS projects;
-DROP TABLE IF EXISTS users_projects;
-DROP TABLE IF EXISTS sprints;
-DROP TABLE IF EXISTS tasks;
-DROP TABLE IF EXISTS users_tasks;
-DROP TABLE IF EXISTS meetings;
-DROP TABLE IF EXISTS users_meetings;
-DROP TABLE IF EXISTS messages;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS oauth_mapping CASCADE;
+DROP TABLE IF EXISTS projects CASCADE;
+DROP TABLE IF EXISTS users_projects CASCADE;
+DROP TABLE IF EXISTS sprints CASCADE;
+DROP TABLE IF EXISTS tasks CASCADE;
+DROP TABLE IF EXISTS users_tasks CASCADE;
+DROP TABLE IF EXISTS meetings CASCADE;
+DROP TABLE IF EXISTS users_meetings CASCADE;
+DROP TABLE IF EXISTS messages CASCADE;
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
@@ -18,58 +18,58 @@ CREATE TABLE users (
 
 CREATE TABLE oauth_mapping (
   id SERIAL PRIMARY KEY NOT NULL,
-  user_id INT REFERENCES users(id) NOT NULL ON DELETE CASCADE,
+  user_id INT REFERENCES users(id) ON DELETE CASCADE,
   oauth_provider TEXT NOT NULL,
   oauth_id INT NOT NULL
 );
 
 CREATE TABLE projects (
   id SERIAL PRIMARY KEY NOT NULL,
-  creator_id REFERENCES users(id) NOT NULL
+  creator_id INT REFERENCES users(id),
   name VARCHAR(255) NOT NULL,
   description TEXT,
   start_date DATE NOT NULL,
   end_date DATE NOT NULL,
   background_img VARCHAR(255) NOT NULL,
-  status TEXT NOT NULL DEFAULT "Incomplete"
+  current_status TEXT DEFAULT 'Incomplete'
 );
 
 CREATE TABLE users_projects (
   id SERIAL PRIMARY KEY NOT NULL,
-  user_id REFERENCES users(id) NOT NULL ON DELETE CASCADE,
-  project_id REFERENCES projects(id) NOT NULL ON DELETE CASCADE,
-  role VARCHAR(255)
+  user_id INT REFERENCES users(id) ON DELETE CASCADE,
+  project_id INT REFERENCES projects(id) ON DELETE CASCADE,
+  role VARCHAR(255),
   UNIQUE (user_id, project_id)
 );
 
 CREATE TABLE sprints (
   id SERIAL PRIMARY KEY NOT NULL,
-  project_id REFERENCES projects(id) NOT NULL ON DELETE CASCADE
-   name VARCHAR(255) NOT NULL,
+  project_id INT REFERENCES projects(id) ON DELETE CASCADE,
+  name VARCHAR(255) NOT NULL,
   description TEXT,
   start_date DATE NOT NULL,
   end_date DATE NOT NULL,
-  status TEXT NOT NULL DEFAULT "Incomplete"
+  current_status TEXT DEFAULT 'Incomplete'
 );
 
 CREATE TABLE tasks (
   id SERIAL PRIMARY KEY NOT NULL,
-  project_id REFERENCES projects(id) NOT NULL ON DELETE CASCADE
-  sprint_id REFERENCES sprints(id) ON DELETE CASCADE
+  project_id INT REFERENCES projects(id) ON DELETE CASCADE,
+  sprint_id INT REFERENCES sprints(id) ON DELETE CASCADE,
   name VARCHAR(255) NOT NULL,
   description TEXT,
   start_date DATE NOT NULL,
   end_date DATE NOT NULL,
   priority_level TEXT NOT NULL,
-  status TEXT NOT NULL DEFAULT "Incomplete"
+  current_status TEXT DEFAULT 'Incomplete'
 );
 
 CREATE TABLE users_tasks (
   id SERIAL PRIMARY KEY NOT NULL,
-  user_id REFERENCES users(id) NOT NULL ON DELETE CASCADE,
-  task_id REFERENCES tasks(id) NOT NULL ON DELETE CASCADE,
+  user_id INT REFERENCES users(id) ON DELETE CASCADE,
+  task_id INT REFERENCES tasks(id) ON DELETE CASCADE,
   UNIQUE (user_id, task_id)
-)
+);
 
 CREATE TABLE meetings (
   id SERIAL PRIMARY KEY NOT NULL,
@@ -82,15 +82,15 @@ CREATE TABLE meetings (
 
 CREATE TABLE users_meetings (
   id SERIAL PRIMARY KEY NOT NULL,
-  user_id REFERENCES users(id) NOT NULL ON DELETE CASCADE,
-  meeting_id REFERENCES meetings(id) NOT NULL ON DELETE CASCADE,
+  user_id INT REFERENCES users(id) ON DELETE CASCADE,
+  meeting_id INT REFERENCES meetings(id) ON DELETE CASCADE,
   UNIQUE (user_id, meeting_id)
 );
 
 CREATE TABLE messages (
   id SERIAL PRIMARY KEY NOT NULL,
   message TEXT NOT NULL,
-  sender_id REFERENCES users(id) NOT NULL,
-  receiver_id REFERENCES users(id)
+  sender_id INT REFERENCES users(id),
+  receiver_id INT REFERENCES users(id)
 );
 

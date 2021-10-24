c43a47ec97158ae3bc52ecc182e9024cc734babb
Carrie <carr1eg@hotmail.com>
2021-10-24 00:14:49 -0700

    update read from database function to include avatar

--- server/src/models/person.ts
+++ server/src/models/person.ts
@@ -8,7 +8,7 @@ function getPerson(id: number) {
 
 function getPersonByGitHub(githubId: number) {
   return pool.query(
-    `SELECT users.id, users.name, oauth_mapping.oauth_id
+    `SELECT users.id, users.name, users.avatar, oauth_mapping.oauth_id
       FROM users
       JOIN oauth_mapping on oauth_mapping.user_id = users.id
       WHERE oauth_mapping.oauth_id = $1`,
@@ -24,16 +24,15 @@ async function createPersonByGitHub(
   const client = await pool.connect();
 
   try {
-    const insertPersonText = //TODO: Update query values
-      "INSERT INTO users(name) VALUES('TEST') RETURNING users.id";
-    const insertedUserId = (await client.query(insertPersonText))
-      .rows[0].id;
+    const insertPersonText = `INSERT INTO users(name, avatar) VALUES('${name}', '${avatar}') RETURNING users.id`;
+    const insertedUserId = (await client.query(insertPersonText)).rows[0].id;
 
     const insertText = `
       INSERT INTO oauth_mapping(user_id, oauth_provider, oauth_id) 
       VALUES($1, $2, $3);
       `;
     const insertValues = [insertedUserId, "GitHub", githubId];
+    console.log("insertValues :>> ", insertValues);
     await client.query(insertText, insertValues);
   } catch (e) {
     throw e;

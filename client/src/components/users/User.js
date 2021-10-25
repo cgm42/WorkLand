e003e0b8b49ac4224cd5d3528ca20f535ff2df36
Johnny <johnnylabedzki@hotmail.com>
2021-10-25 15:43:37 -0700

    Add user component class based on value of selected

--- client/src/components/users/User.js
+++ client/src/components/users/User.js
@@ -1,9 +1,16 @@
 import React from "react";
+import classNames from "classnames";
 
 export default function User(props) {
-  const {id, avatar, name} = props;
+  const {id, avatar, name, selected} = props;
+
+  const userClass = classNames(
+    "user-list",
+    {"user-list--selected": selected }
+  )
+
   return (
-    <li id={id}>
+    <li className={userClass} id={id}>
       <img src={avatar}/>
       <p>{name}</p>
     </li>

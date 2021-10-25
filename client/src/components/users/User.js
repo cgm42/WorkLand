d6d7481bb0b5570bc6fa956244acd047013719e3
Josebautista10 <cooldj1020@gmail.com>
2021-10-25 16:56:59 -0600

    styled users list

--- client/src/components/users/User.js
+++ client/src/components/users/User.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function User(props) {
   const {id, avatar, name} = props;
   return (
-    <li id={id}>
+    <li id={id} className="user-list">
       <img src={avatar}/>
       <p>{name}</p>
     </li>

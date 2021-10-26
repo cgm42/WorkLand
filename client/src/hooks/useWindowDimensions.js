e0efff5a7eec0cd3c5503d663841c7717c9892d3
Carrie <carr1eg@hotmail.com>
2021-10-26 12:17:26 -0700

    add a hook to read window size

---
+++ client/src/hooks/useWindowDimensions.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height,
+  };
+}
+
+export default function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowDimensions;
+}

618fe728bdba3bef4a8ab3a5f6f17058c7dba1ad
Johnny <johnnylabedzki@hotmail.com>
2021-10-26 20:11:13 -0700

    install DND

--- client/src/components/kanban/KanbanTaskItem.js
+++ client/src/components/kanban/KanbanTaskItem.js
@@ -1,24 +1,26 @@
-import React from 'react'
-import { AiFillWarning } from 'react-icons/ai';
+import React from "react";
+import { AiFillWarning } from "react-icons/ai";
 
 function KanbanTaskItem() {
   return (
-    <div className='task-item-container'>
-      <div className='task-item-header'>
-        <h1>title</h1>
-        <div className='priority-icon'>
+    <ul className="task-item-container">
+      <li>
+        <div className="task-item-header">
+          <h1>title</h1>
+          <div className="priority-icon">
+            <AiFillWarning></AiFillWarning>
+          </div>
+        </div>
+
+        <p>description</p>
+        <div className="task-users">
+          <AiFillWarning></AiFillWarning>
+          <AiFillWarning></AiFillWarning>
           <AiFillWarning></AiFillWarning>
         </div>
-      </div>
-
-      <p>description</p>
-      <div className='task-users'>
-        <AiFillWarning></AiFillWarning>
-        <AiFillWarning></AiFillWarning>
-        <AiFillWarning></AiFillWarning>
-      </div>
-    </div>
-  )
+      </li>
+    </ul>
+  );
 }
 
-export default KanbanTaskItem
+export default KanbanTaskItem;

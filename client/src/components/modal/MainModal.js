82abe1918ade3fc88e63e7fbbb51c7befd9d48f1
Carrie <carr1eg@hotmail.com>
2021-10-27 00:05:56 -0700

    add kanban and user dashboard route on map

--- client/src/components/modal/MainModal.js
+++ client/src/components/modal/MainModal.js
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
-import Modal from "react-modal";
-import { useDispatch, useSelector } from "react-redux";
-import { TOGGLE_MODAL_CAN_OPEN } from "../../reducers/mapReducer";
-import "./modal.css";
-import UserDashboard from "../userDashboard/UserDashboard";
+import React, { useState } from 'react';
+import Modal from 'react-modal';
+import { useDispatch, useSelector } from 'react-redux';
+import { TOGGLE_MODAL_CAN_OPEN } from '../../reducers/mapReducer';
+import './modal.css';
+import UserDashboard from '../userDashboard/UserDashboard';
 import ProjectCardList from '../projects/projectCardList';
-
+import Kanban from '../kanban/Kanban';
 function MainModal(props) {
   const dispatch = useDispatch();
   const [modalIsOpen, setModalIsOpen] = useState(props.isOpen);
@@ -20,13 +20,12 @@ function MainModal(props) {
           }}>
           close
         </button>
-        {mapRoute === "kanban" ? <div>kanban</div> : <div />}
-        <div>
-          <UserDashboard />
-        </div>
-        <div>
-          <ProjectCardList  />
-        </div>
+        {mapRoute === 'userDashboard' ? <UserDashboard /> : <div />}
+        {mapRoute === 'kanban' ? <Kanban /> : <div />}
+
+        {/* <div>
+          <ProjectCardList />
+        </div> */}
       </Modal>
     </>
   );

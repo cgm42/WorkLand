d69d4cb645143fa2d4bd181760ed61db9813cf3e
Josebautista10 <cooldj1020@gmail.com>
2021-10-30 13:43:30 -0600

    got background for just login instead of HTML

--- client/src/index.js
+++ client/src/index.js
@@ -1,25 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import StateProvider from "./components/providers/StateProvider";
-import { BrowserRouter } from "react-router-dom";
-import { configureStore } from "@reduxjs/toolkit";
-import { Provider } from "react-redux";
-import { mapReducer } from "./reducers/mapReducer";
-import { socketRTK } from "./middleware/socketRTK";;
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import StateProvider from './components/providers/StateProvider';
+import { BrowserRouter } from 'react-router-dom';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import { mapReducer } from './reducers/mapReducer';
+import { socketRTK } from './middleware/socketRTK';
 
 const store = configureStore({
   reducer: mapReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(socketRTK()),
+    getDefaultMiddleware().concat(socketRTK())
 });
-
 
 ReactDOM.render(
   <BrowserRouter>
     <Provider store={store}>
       <App />
+
     </Provider>
   </BrowserRouter>,
-  document.getElementById("root")
+  document.getElementById('root')
 );

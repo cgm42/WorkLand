ce829aa309768717b7bc0d5c17d8d78406dbb95c
Carrie <carr1eg@hotmail.com>
2021-11-01 11:02:14 -0700

    add credential to request option

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -9,7 +9,7 @@ import { BsGithub } from 'react-icons/bs';
 const Login = () => {
   const dispatch = useDispatch();
   useEffect(() => {
-    axios.get('/user').then((data) => {
+    axios.get('/user', { withCredentials: true }).then((data) => {
       console.log(data);
       dispatch(SET_USER(data.data));
     });
@@ -21,28 +21,28 @@ const Login = () => {
   });
   console.log(process.env);
   return (
-    <div className='background'>
+    <div className="background">
       {userState.name !== '' ? (
         <div></div>
       ) : (
         <>
-          <div className='login-container'>
-            <div className='login'>
-              <BsGithub className='github' />
+          <div className="login-container">
+            <div className="login">
+              <BsGithub className="github" />
               <a href={process.env.REACT_APP_GITHUB_LOGIN}>
                 {' Log in with Github'}
               </a>
             </div>
           </div>
-          <div className='stars-login'>
-            <div id='stars'></div>
-            <div id='stars2'></div>
-            <div id='stars3'></div>
+          <div className="stars-login">
+            <div id="stars"></div>
+            <div id="stars2"></div>
+            <div id="stars3"></div>
           </div>
         </>
       )}
 
-      {userState.name !== '' ? <a href='/logout'>logout</a> : <div> </div>}
+      {userState.name !== '' ? <a href="/logout">logout</a> : <div> </div>}
     </div>
   );
 };

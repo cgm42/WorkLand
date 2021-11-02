fb1e473ba9e8eaaaf1c798cf97cd210108aab61c
Carrie <carr1eg@hotmail.com>
2021-11-01 22:03:22 -0700

    acomplete about project with tech stack used and add our profile, github and linkedin links

--- client/src/components/userDashboard/UserDashboard.js
+++ client/src/components/userDashboard/UserDashboard.js
@@ -25,37 +25,124 @@ function UserDashboard() {
   }, []);
 
   const userState = useSelector((state) => {
-    console.log('state:', state);
+    // console.log('state:', state);
     return state.user;
   });
 
   return (
-    <div className='dashboard-layout nes-container is-rounded is-dark'>
-      <div className='welcome'>
-        <h1>{userState.name}'s Dashboard </h1>
-        <hr></hr>
+    <div
+      style={{ display: 'flex', alignItems: 'center' }}
+      className="dashboard-layout nes-container is-rounded is-dark">
+      <div className="welcome">
+        <h1>About</h1>
+        <hr style={{ width: '200px' }}></hr>
       </div>
 
-      <section className='user-welcome'>
-        <h1>Welcome To WorkLand</h1>
-        <p>
-          WorkLand is a place that allows you to have a fun environment while
-          being at work. It's a project management app unlike others, it allows
-          you to see your projects, tasks, schedule but not the way you are used
-          to. We want you to feel like you're not using a workplace app. So to
-          do that we made the whole app with an 8-bit font and also gave you the
-          option to choose a character to give it that game like feel.
-        </p>
-      </section>
-
-      <section className='level-chart-cards'>
-        <div className='user-progress nes-container is-rounded'>
-          <div className='user'>
-            <img src={userState.avatar} className='user-img'></img>
-            <div className='user-info-text'>
-              <p>{userState.name}</p>
-            </div>
+      <div className="user-progress nes-container is-rounded">
+        <div className="user">
+          <img src={userState.avatar} className="user-img"></img>
+          <div
+            style={{ display: 'flex', flexDirection: 'column' }}
+            className="user-info-text">
+            <p>{userState.name}</p>
+            <p>user #{userState.id}</p>
           </div>
+        </div>
+      </div>
+
+      <section className="user-welcome">
+        <h1>Thanks for visiting us!</h1>
+        <p>
+          WorkLand is a virtual workspace with built in project management tools
+          designed for tech teams.
+        </p>
+        <span />
+        <hr style={{ width: '200px' }}></hr>
+        <h3>Built with: </h3>
+        <p>
+          JavaScript TypeScript React Redux Websocket Node Express PostgreSQL
+        </p>
+        <hr style={{ width: '200px' }}></hr>
+
+        <div style={{ display: 'flex' }}>
+          <section className="nes-container is-dark member-card">
+            <div className="avatar">
+              <img
+                style={{ width: '200px' }}
+                alt="profile pic"
+                src="	https://avatars.githubusercontent.com/u/38818956?v=4"
+                target="_blank"
+              />
+            </div>
+            <div className="profile">
+              <h4 className="name">{'Carrie G'}</h4>
+              <div>
+                <a
+                  href="https://github.com/cgm42"
+                  rel="noopener noreferrer"
+                  target="_blank">
+                  <i class="nes-icon github"></i>
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/carriegeng/"
+                  rel="noopener noreferrer"
+                  target="_blank">
+                  <i class="nes-icon linkedin"></i>
+                </a>
+              </div>
+            </div>
+          </section>
+
+          <section className="nes-container is-dark member-card">
+            <div className="avatar">
+              <img
+                style={{ width: '200px' }}
+                alt="profile pic"
+                src="	https://avatars.githubusercontent.com/u/86113795?v=4"
+              />
+            </div>
+            <div className="profile">
+              <h4 className="name">{'Johnny L'}</h4>
+              <div>
+                <a
+                  href="https://github.com/jlabedzki"
+                  rel="noopener noreferrer"
+                  target="_blank">
+                  <i class="nes-icon github"></i>
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/johnny-labedzki-5bb4b220a/"
+                  rel="noopener noreferrer"
+                  target="_blank">
+                  <i class="nes-icon linkedin"></i>
+                </a>
+              </div>
+            </div>
+          </section>
+
+          <section className="nes-container is-dark member-card">
+            <div className="avatar">
+              <img
+                style={{ width: '200px' }}
+                alt="profile pic"
+                src="	https://avatars.githubusercontent.com/u/82417707?v=4"
+              />
+            </div>
+            <div className="profile">
+              <h4 className="name">{'Jose B'}</h4>
+              <div>
+                <a
+                  href="https://github.com/Josebautista10"
+                  rel="noopener noreferrer"
+                  target="_blank">
+                  <i class="nes-icon github"></i>
+                </a>
+                <a>
+                  <i class="nes-icon linkedin"></i>
+                </a>
+              </div>
+            </div>
+          </section>
         </div>
       </section>
     </div>

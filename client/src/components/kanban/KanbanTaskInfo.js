8dadaa8a9058222e59e17ef7b49dbf3e312ba93c
Josebautista10 <cooldj1020@gmail.com>
2021-11-01 19:24:05 -0600

    displaying correct definition with task

--- client/src/components/kanban/KanbanTaskInfo.js
+++ client/src/components/kanban/KanbanTaskInfo.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { BiShow } from 'react-icons/bi';
 
+
 function KanbanTaskInfo(props) {
+  const taskItemId = `dialog-dark-${props.id}`
+  
   return (
     <div>
       <BiShow
         type='button'
         className='show-more'
         onClick={() => {
-          document.getElementById('dialog-dark').showModal();
+          document.getElementById(taskItemId).showModal();
         }}
       ></BiShow>
-      <dialog className='nes-dialog is-dark task-dialog' id='dialog-dark'>
+      <dialog className='nes-dialog is-dark task-dialog' id={taskItemId}>
         <form method='dialog'>
           <h1>Task Name:</h1>
           <p className='title'>{props.title}</p>
